35973935a50c5b890318ca2f3bee9db8d6c57009
GABBRIELJUA <20221040@uthh.edu.mx>
2025-09-10 16:25:27 -0600

    Agrega componente Area simple

---
+++ conversor-unidades/src/Components/Area.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Area() {
+  return (
+    <div>
+      <h2>Cálculo de Área</h2>
+      <p>Aquí se calcula el área.</p>
+    </div>
+  );
+}
+
+export default Area;
