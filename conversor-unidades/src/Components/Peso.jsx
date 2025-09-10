fffd9a463cfae520b482106649591e416db822cc
LUISGERARDO11 <luis34julieta@gmail.com>
2025-09-10 16:29:57 -0600

    integracion de la funcionalidad de conversion para unidades de peso

---
+++ conversor-unidades/src/Components/Peso.jsx
@@ -0,0 +1,12 @@
+// src/components/MiComponente.jsx
+import React from "react";
+
+const MiComponente = () => {
+  return (
+    <div>
+      <h2>Hola, soy un componente con conversion de peso</h2>
+    </div>
+  );
+};
+
+export default MiComponente;
