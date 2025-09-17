740c3c9e057dd691ac42d4b8b8f74f187c74c92b
Fatima Pardo Beltran <20221024@uthh.edu.mx>
2025-09-17 14:47:00 -0600

    Agrega componente simple de longitud

---
+++ conversor-unidades/src/Components/Longitud.jsx
@@ -0,0 +1,7 @@
+export default function Volumen() {
+  return (
+    <div>
+      <h2>Esta es mi historia de usuario, referente a la conversion de unidades referente a la longitud</h2>
+    </div>
+    );
+}
